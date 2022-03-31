984646606a7509473396334f1e60b2bb64f1bbc5
olrodr03 <98947628+olrodr03@users.noreply.github.com>
2022-03-31 14:52:41 +0200

    Update tunel_básico_22.py

--- tunel_básico_22.py
+++ tunel_básico_22.py
@@ -17,7 +17,7 @@ from multiprocessing import Value
 SOUTH = "north"
 NORTH = "south"
 
-NCARS = 100
+NCARS = 10
 
 class Monitor():
     def __init__(self):
@@ -92,25 +92,19 @@ def car(cid, direction, monitor):
     print(f"car {cid} heading {direction} wants to enter")
     monitor.wants_enter(direction)
     print(f"car {cid} heading {direction} enters the tunnel")
-    if direction==NORTH:
-       delay(3)
-    else:
-       delay(6)
     print(f"car {cid} heading {direction} leaving the tunnel")
     monitor.leaves_tunnel(direction)
     print(f"car {cid} heading {direction} out of the tunnel")
-
-
 
 def main():
     monitor = Monitor()
     cid = 0
     for _ in range(NCARS):
-        direction = NORTH if random.randint(0,1)==1  else SOUTH
+        direction = NORTH if random.randint(0, 1) == 1 else SOUTH
         cid += 1
-        p = Process(target=car, args=(cid, direction, monitor))
+        p = Process(target = car, args = (cid, direction, monitor))
         p.start()
-        time.sleep(random.expovariate(1/0.5)) # a new car enters each 0.5s
+        time.sleep(random.expovariate(1 / 0.5)) # Un nuevo coche entra cada 0.5 segundos.
 
 if __name__ == '__main__':
     main()
